c54b6a9c16e14548360f5087374892979bd91363
kiet.pham.leaneo <kiet.pham@leaneo.com>
2023-02-26 19:18:40 +0700

    ....

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ mongoose
 app.use("/api/", mainRoutes);
 
 // set up port
-const port = 5035;
+const port = process.env.PORT || "3000";
 // set up route
 app.get("/", (req, res) => {
     res.status(200).json({
