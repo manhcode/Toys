06e899ff670527139f33deb53f6ea2639937386e
kiet.pham.leaneo <kiet.pham@leaneo.com>
2023-02-27 00:32:33 +0700

    ...

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,19 +11,7 @@ router.get("/", async function (req, res, next) {
                 : "http://localhost:3000/"
         }api/toys`
     );
-    res.render("index", { toys: toys.data.Toy });
-});
-
-router.patch("/:toyId", async function (req, res, next) {
-    const toys = await axios.patch(
-        `${
-            process.env.NODE_ENV
-                ? "https://apptoy.herokuapp.com/"
-                : "http://localhost:3000/"
-        }api/toys/${req.params.toyId}`,
-        req.body
-    );
-    res.render("index", { toys: toys.data.Toy });
+    res.render("index", { toys: toys.data.Toy, env: process.env.NODE_ENV });
 });
 
 module.exports = router;
