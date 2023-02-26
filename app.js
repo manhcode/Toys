09244cd3da0801d90d9696d70a6886b549cb6d11
@nv_truonqqq <75243419+truongnvgcd201597@users.noreply.github.com>
2023-02-26 20:33:47 +0700

    make it better

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mongoose
         { useNewUrlParser: true, useUnifiedTopology: true }
     )
     .then(() => {
-        console.log("Database connecteddd");
+        console.log("Database connected");
     })
     .catch((error) => {
         console.log("Error connecting to database");
